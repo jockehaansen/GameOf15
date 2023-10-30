91910af9d3b802a57daf001ec51a665b5455db64
Joakim Hansen <86515384+Lizziq@users.noreply.github.com>
2023-10-30 09:58:45 +0100

    First commit, GameWindow class & TileGenerator class created

---
+++ src/GameWindow.java
@@ -0,0 +1,31 @@
+public class GameWindow {
+
+    public GameWindow(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
---
+++ src/TileGenerator.java
@@ -0,0 +1,6 @@
+public class TileGenerator {
+
+    public TileGenerator(){
+
+    }
+}
